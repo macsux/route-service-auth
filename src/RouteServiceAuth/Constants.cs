253f35f47a0a4f3570d9ba45bb7e0bc9a9873f4b
Andrew Stakhov <andrew.stakhov@gmail.com>
2020-06-03 12:28:51 -0400

    Fix

--- src/RouteServiceAuth/Constants.cs
+++ src/RouteServiceAuth/Constants.cs
@@ -3,7 +3,7 @@ namespace RouteServiceAuth
     public static class Constants
     {
         public const string X_CF_Forwarded_Url = "X-CF-Forwarded-Url";
-        public const string X_CF_Identity = "X_CF_Identity";
+        public const string X_CF_Identity = "X-CF-Identity";
         public const string X_CF_Roles = "X-CF-Roles";
     }
 }
